f9b557e01e1a17b788cac2a20671d827e290931b
vishal0930 <vishal09121998@gmail.com>
2022-05-28 01:06:31 +0530

    Fixed asset pipeline issue

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,4 +1,5 @@
 //= link_tree ../images
 //= link_directory ../stylesheets .css
-//= link_tree ../../javascript .js
-//= link_tree ../../../vendor/javascript .js
+//= link_directory ../javascripts .js
+//= link_tree ../../../vendor/assets/javascripts .js
+//= link_tree ../../../vendor/assets/stylesheets .scss
